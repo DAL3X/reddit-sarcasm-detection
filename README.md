337cd4518b7846fb0713ede8539c568591a3d861
philipsch98 <philipschr1998@gmail.com>
2022-02-11 18:15:54 +0100

    update

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ Machine learning project for 'laboratory for applied machine learning algorithms
 ![results](https://github.com/phil-kit/reddit-comment-lama/raw/main/graphs/compare_models.png)
 
 ## proof of concept - real world implementation
-* run 'am_i_sarcastic.py' for a simple demonstration. It uses 'models/only_comment' since we don't have any metadata for text input.
+* run 'am_i_sarcastic.py' for a simple demonstration. It uses 'models/full_only_comment' since we don't have any metadata for text input.
 
 ![example](https://github.com/phil-kit/reddit-comment-lama/raw/main/graphs/am_i_sarcastic.png)
 
--- am_i_sarcastic.py
+++ am_i_sarcastic.py
@@ -1,4 +1,4 @@
-# example for real world implementation. proof of concept
+# simple example for real world implementation. proof of concept
 
 print('starting...')
 from os import environ
@@ -6,14 +6,14 @@ environ["CUDA_VISIBLE_DEVICES"] = "-1" # disable GPU for simple prediction
 from tensorflow.keras.models import load_model
 
 print('loading model for prediction...')
-model = load_model(f'models/only_comment') #load model
+model = load_model('models/full_only_comment') #load model
 print('done.\n')
 
 while True:
     print('\033[95menter yout text:\033[94m ', end='')
-    text = input() # prompt for input
-    if (text != ''): # prediction crashes with empty string
-        prediction = model.predict([text])[0][0] # get prediction
-        sarcasm = True if (prediction>.5) else False
+    text = input()      # prompt for input
+    if (text != ''):    # prediction crashes with empty string
+        prediction = model.predict([text])[0][0]      # get prediction
+        sarcasm = True if (prediction>.5) else False  # classify
         if sarcasm: print(f'\033[95m> your input was sarcasm (confidence: {int(prediction*100)}%)\033[94m\n')
         else: print(f'\033[95m> your input was not sarcasm (confidence: {int((1-prediction)*100)}%)\033[94m\n')
