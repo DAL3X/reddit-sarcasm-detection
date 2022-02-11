cdd20868298dc077665644e0e519c80b5fe0dd2a
philipsch98 <philipschr1998@gmail.com>
2022-02-11 14:50:54 +0100

    am_i_sarcastic

--- am_i_sarcastic.py
+++ am_i_sarcastic.py
@@ -1,25 +1,19 @@
 # example for real world implementation. proof of concept
 
 print('starting...')
-
-# imports
-import tensorflow as tf
-from typing import *
-
-# enables GPU VRAM growth, static allocation tends to crash Desktop programs
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+from os import environ
+environ["CUDA_VISIBLE_DEVICES"] = "-1" # disable GPU for simple prediction
+from tensorflow.keras.models import load_model
 
 print('loading model for prediction...')
-model = tf.keras.models.load_model(f'models/only_comment')
+model = load_model(f'models/only_comment') #load model
 print('done.\n')
 
 while True:
     print('\033[95menter yout text:\033[94m ', end='')
-    text = input()
-    if (text != ''):
-        prediction = model.predict([text])[0][0]
+    text = input() # prompt for input
+    if (text != ''): # prediction crashes with empty string
+        prediction = model.predict([text])[0][0] # get prediction
         sarcasm = True if (prediction>.5) else False
-        if sarcasm: print(f'\033[95m> your input was sarcasm (confidence: {int(prediction*100)}%)\033[94m')
-        else: print(f'\033[95m> your input was not sarcasm (confidence: {int((1-prediction)*100)}%)\033[94m')
+        if sarcasm: print(f'\033[95m> your input was sarcasm (confidence: {int(prediction*100)}%)\033[94m\n')
+        else: print(f'\033[95m> your input was not sarcasm (confidence: {int((1-prediction)*100)}%)\033[94m\n')
